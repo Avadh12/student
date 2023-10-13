65ad55e4e5de31221495b055baf952d8bc3160a9
avadhsharma010 <147719208+avadhsharma010@users.noreply.github.com>
2023-10-13 14:25:12 +0530

    Update StudentEntityWsApplicationTests.java
    event element

--- student-ws/src/test/java/com/shredder/studentws/StudentEntityWsApplicationTests.java
+++ student-ws/src/test/java/com/shredder/studentws/StudentEntityWsApplicationTests.java
@@ -9,5 +9,20 @@ class StudentEntityWsApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	public class PrintEvenCharacters {
+    public static void main(String[] args) {
+        String input = "I love my country";
+        printEvenCharacters(input);
+    }
+
+    private static void printEvenCharacters(String input) {
+        for (int i = 0; i < input.length(); i += 2) {
+            char character = input.charAt(i);
+            if (character != ' ') {
+                System.out.print(character);
+            }
+        }
+    }
+}
 
 }
